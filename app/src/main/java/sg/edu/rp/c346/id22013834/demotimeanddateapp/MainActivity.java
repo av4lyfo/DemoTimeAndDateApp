be91a45c78525cb5284615de873dd03a65a38e54
av4lyfo <22013834@myrp.edu.sg>
2023-06-26 18:58:22 +0800

    nil

--- app/src/main/java/sg/edu/rp/c346/id22013834/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22013834/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,73 @@ package sg.edu.rp.c346.id22013834.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
+    Button btnReset;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate= findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay  = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.buttonReset);
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(tp.getCurrentMinute()<10) {
+                    tvDisplay.setText("Time is " + tp.getCurrentHour() + "." + "0"+tp.getCurrentMinute());
+                }else{
+                    tvDisplay.setText("Time is " + tp.getCurrentHour() + "." + tp.getCurrentMinute());
+                }
+
+
+
+            }
+        });
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvDisplay.setText("Date is"+ dp.getDayOfMonth()+"/"+dp.getMonth()+1+"/"+dp.getYear());
+
+
+
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                int day=01;
+                int month=0;
+                int year=2020;
+                dp.updateDate(year,month,day);
+
+
+
+            }
+        });
+
+
+
     }
 }
